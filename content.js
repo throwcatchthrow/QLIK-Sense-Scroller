2830d116ceb197f3aeb8d8743245a2cf3010f4db
throwcatchthrow <throwcatchthrow@gmail.com>
2018-08-08 15:58:00 -0400

    Fixed Timer Issues
    Bugs were that the timer settings were being ignore and run every few milliseconds. Fixed by converting setInterval to a recursive setTimer call.

--- content.js
+++ content.js
@@ -22,63 +22,83 @@ chrome.storage.sync.get(['qlik_refresh_interval'], function(data) {
 	if (data.qlik_refresh_interval == null) {
 		chrome.storage.sync.set({'qlik_refresh_interval':60}, function(){
 			console.log('Default Inverval to 60 Seconds');
-			refresh_rate = 60;
+			refresh_rate = 60*1000;
 		});
 	} else {
-		refresh_rate = data.qlik_refresh_interval
-		console.log('Refresh Setting: ' + data.qlik_refresh_interval)
+		refresh_rate = data.qlik_refresh_interval * 1000
+		console.log('Refresh Setting: ' + refresh_rate)
 	}
 	
 });
 
 // Handles the Looping Interval
-setInterval(
-	function(){
 
-		// Creates variables for the previous and next buttons in QLIK Sense
-		var nxt = document.querySelector("button[tid='btnQuickNavNext']");
-		var prv = document.querySelector("button[tid='btnQuickNavPrevious']");
-	 	if ((nxt != null)||(prv != null)){
+function periodicall(){
 
-	 			// Handles the On/Off Parameter
-				chrome.storage.sync.get(['qlik_scrolling'], function(data) {
-					if (data.qlik_scrolling == null) {
-						chrome.storage.sync.set({'qlik_scrolling':false}, function(){
-							execute_scroll = false;
-						});
-					} else {
-						execute_scroll = data.qlik_scrolling
-					}
-					
+	// Creates variables for the previous and next buttons in QLIK Sense
+	var nxt = document.querySelector("button[tid='btnQuickNavNext']");
+	var prv = document.querySelector("button[tid='btnQuickNavPrevious']");
+ 	if ((nxt != null)||(prv != null)){
+
+		// Handles the On/Off Parameter
+		chrome.storage.sync.get(['qlik_scrolling'], function(data) {
+			if (data.qlik_scrolling == null) {
+				chrome.storage.sync.set({'qlik_scrolling':false}, function(){
+					execute_scroll = false;
 				});
+			} else {
+				execute_scroll = data.qlik_scrolling
+			}
 			
-			if((document.getElementById("show-service-popup-dialog")!=null)) {	
-				location.reload();
-			}
-			// Checks if scroll should be currently executing
-			else if (execute_scroll) {
-
-				// Validates scrolling direction and when it should be reversed
-				if ((direction_forward && nxt.disabled)||(!direction_forward && prv.disabled)) {
-					direction_forward = !(direction_forward);
-				}
-
-				// Based on direction, pushes element forward or backward.
-				if (direction_forward) {
-					MouseEventSequence(nxt);
-					console.log("Move Forward");
-				} else {
-					MouseEventSequence(prv);
-					console.log("Move Backward");
-				}
-			} else{
-				console.log("Scrolling Is Paused.")
-			}
+		});
+		
+		if((document.getElementById("show-service-popup-dialog")!=null)) {	
+			location.reload();
 		}
-		else {
-			console.log("Next/Previous Button Could Not Be Found");
+		// Checks if scroll should be currently executing
+		else if (execute_scroll) {
+
+			// Validates scrolling direction and when it should be reversed
+			if ((direction_forward && nxt.disabled)||(!direction_forward && prv.disabled)) {
+				direction_forward = !(direction_forward);
+			}
+
+			// Based on direction, pushes element forward or backward.
+			if (direction_forward) {
+				MouseEventSequence(nxt);
+				console.log("Move Forward");
+			} else {
+				MouseEventSequence(prv);
+				console.log("Move Backward");
+			}
+		} else{
+			console.log("Scrolling Is Paused.")
 		}
-	},(10000));
+	}
+	else {
+		console.log("Next/Previous Button Could Not Be Found");
+	}
+
+	// Checks if Timeout timer has changed
+
+	setTimeout(periodicall, refresh_rate);
+	chrome.storage.sync.get(['qlik_refresh_interval'], function(data) {
+		if (data.qlik_refresh_interval == null) {
+			chrome.storage.sync.set({'qlik_refresh_interval':60}, function(){
+				console.log('Default Inverval to 60 Seconds');
+				refresh_rate = 60*1000;
+			});
+		} else {
+			refresh_rate = data.qlik_refresh_interval * 1000
+			console.log('Refresh Setting: ' + refresh_rate)
+		}
+	
+	});
+};
+
+periodicall();
+
+
 
 
 // Handles the Mouse Event Sequence
